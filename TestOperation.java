d3704f569759f9a7a884252e704ba94ab4b387b2
Danyaal Akhtar <danyaal.akhtar@edu.univ-paris13.fr>
2024-06-02 13:35:55 +0200

    Corrections de bugs

--- TestOperation.java
+++ TestOperation.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 public class TestOperation {
 
     public static void main(String[]args){
@@ -12,19 +12,4 @@ public class TestOperation {
         System.out.println(d + " = " + d.valeur()) ; // affiche ((17 - 2) / (2 + 3)) = 3
         
     }
-=======
-public class TestOperation {
-
-    public static void main(String[]args){
-        
-        Expression deux = new Nombre(2) ; 
-        Expression trois = new Nombre(3) ; 
-        Expression dixSept = new Nombre(17) ; 
-        Expression s = new Soustraction(dixSept, deux) ; 
-        Expression a = new Addition(deux, trois) ; 
-        Expression d = new Division(s, a) ; 
-        System.out.println(d + " = " + d.valeur()) ; // affiche ((17 - 2) / (2 + 3)) = 3
-        
-    }
->>>>>>> 69a0d98fedb432bd63a3feca652a9d67f5a767f6
 }
